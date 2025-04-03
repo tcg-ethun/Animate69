ab2e34c962535b3b44fd003919229f6dcc1ba2f6
Esan <ethuntcg@gmail.com>
2025-04-03 20:12:17 +0600

    Update

--- Photo/nature.js
+++ Photo/nature.js
@@ -3,15 +3,15 @@
  // Sample photo data (replace with your real data)
  const photos = [
     {
-        src: './Raw/Nature/pic5.png',
+        src: './Raw/Nature/pic2.png',
         category: 'nature'
     },
     {
-          src: './Raw/Nature/pic6.png',
+          src: './Raw/Nature/pic3.png',
         category: 'nature'
     },
     {
-       src: './Raw/Nature/pic7.png',
+       src: './Raw/Nature/pic4.png',
         category: 'nature'
     },
     
